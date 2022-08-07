68cfd1bdf5e8a757e6fb8bf85277442086dfe294
BrunoCostaPrado <brunocostaprado@gmail.com>
2022-08-07 02:34:02 -0300

    .

---
+++ src/pages/button.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/index.module.css";
+
+export default function Home(): JSX.Element {
+  return (
+    <div className={styles.text}>
+      <h1>Hello</h1>
+    </div>
+  );
+}
